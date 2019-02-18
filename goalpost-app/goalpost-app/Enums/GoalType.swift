775076ebb144d7ef1efb0907d51467fbb0c37eed
Adam Ballif <adamballif@Adams-15-MacBook-Pro.local>
2019-02-17 23:56:41 -0700

    static data setup

---
+++ goalpost-app/goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Adam Ballif on 2/17/19.
+//  Copyright © 2019 Adam Ballif. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
